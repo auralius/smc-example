a7369ae58f3448a29ce17cce6907d91f1b87f251
Auralius Manurung <manurung.auralius@gmail.com>
2015-08-10 17:40:13 +0200

    Small value here means more switching happens!

--- run_simu.m
+++ run_simu.m
@@ -13,7 +13,29 @@ ts = 0.001;
 sim('smc_mass_1d.slx')
 
 %% Phase plot of e vs edot
+figure;
 plot(phaseplot(:,1), phaseplot(:,2),'.')
 xlabel('e')
 ylabel('edot');
 title('Phase plot')
+
+%% Tracking
+figure;
+hold on;
+t = 0:ts:(size(tracking)-1)*ts;
+plot(t, tracking(:,1), '-.r', 'Linewidth', 2)
+plot(t, tracking(:,2), 'b')
+xlabel('Time (s)')
+ylabel('X (m)');
+title('Tracking performance')
+legend('reference', 'actual')
+
+%% Switching action
+figure;
+hold on;
+t = 0:ts:(size(tracking)-1)*ts;
+plot(t, switching(:,1))
+xlabel('Time (s)')
+ylabel('Switching action (N)')
+title('Switching action')
+
